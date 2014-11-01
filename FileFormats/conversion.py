ac375cef51469d90ee5b1775f3bda302dd965e1d
hakuva <sampatisai93@gamil.com>
2014-11-01 00:52:26 -0400

    added conversion file

--- FileFormats/conversion.py
+++ FileFormats/conversion.py
@@ -1,6 +1,18 @@
-import yaml
+import csv
 import json
 
-file = open('GpsFilePoints.yml')
-data = yaml.load(file)
-print json.dumps(data, sort_keys=True,indent=4, separators=(',', ': '))
+# Open Input and Output files.
+inputfile = open('GpsFilePoints.csv', 'r')
+outputfile = open('GpsFilePoints.json', 'w')
+
+# Fields present in the CSV file. 
+fields = ("UserID","FieldID","PointID","Lat","Lng","Ele","Time","Course","Hdop"$
+
+# Read all the lines present in CSV file.
+lines = csv.reader(inputfile, fields)
+# Make the input read as JSON. 
+json_out = json.dumps([row for row in lines])
+
+# print to an output file.
+outputfile.write(json_out)
+
